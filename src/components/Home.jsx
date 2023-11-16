12fe47d93ee6acc1c2014e2330102ea695b2c2fb
charmingdarling <h1hasinai_f06@yahoo.com>
2023-11-16 08:58:46 -0800

    changing properties in parallax, adding content within main-content div, playing with height to make it look better

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,37 @@
 import { BiDownArrow } from "react-icons/bi";
 import { Link } from "react-router-dom"; // Importing a Link component; Link component is used to create links to different routes
 import { Parallax } from "react-parallax";
+import { IconContext } from "react-icons";
 
 const Home = () => {
-  return(
-    <div>
-      <Parallax blur={10} bgImage="spaceimage.jpg" bgImageAlt="the cat" strength={350}>
-        <h1 style={{textAlign:"center", marginTop:"20%"}}>Hello, I'm <span>Kim</span>.</h1>
-        <h3 style={{textAlign:"center", marginTop:"20%"}}>
-          <a href="#main-content"><BiDownArrow /></a>
-        </h3>
-        <div style={{ height: '80vh' }} />
-      </Parallax>
-      <div id="main-content">
-        <div style={{ height: '100vh' }} />
+  return (
+    <>
+      <div className="container">
+        <Parallax
+          blur={0}
+          bgImage="mountains.jpg"
+          bgImageAlt="mountains"
+          strength={450}
+        >
+          <h1 style={{ textAlign: "center", marginTop: "20%" }}>
+            Hello, I'm <span>Kim</span>.
+          </h1>
+          <h2 style={{ textAlign: "center" }}>Web Developer</h2>
+          <h3 style={{ textAlign: "center", marginTop: "20%" }}>
+            <a href="#main-content">
+              <BiDownArrow />
+            </a>
+          </h3>
+          <div style={{ height: "0vh" }} />
+        </Parallax>
+
+        <div id="main-content">
+          <div style={{ height: "10vh" }} />
+          <h2 style={{ textAlign: "center" }}>GET TO KNOW ME</h2>
+        </div>
       </div>
-    </div>
+      {/* end container */}
+    </>
   );
 };
 
